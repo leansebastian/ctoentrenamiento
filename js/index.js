e0b175666fcd0d31286d169fb7688d456196bf30
leansebastian <lic.leansebastian@gmail.com>
2023-04-10 22:38:43 -0300

    version 2.0 js

--- js/index.js
+++ js/index.js
@@ -1,80 +1,71 @@
-let continuar = true
+const carrito = []
 
-const mensajeInicial = "Ingresa el código del servicio a consultar: \n" +
-    "a) Online training \n" +
-    "b) Outdoor training \n" +
-    "c) Healthy Fit Program \n" +
-    "d) Entrenamiento Personalizado \n"
+const servicios = [{ imagen: '🍀', codigo: 1, tipo: 'Outdoor training', clase: '(pack 8 clases)', valor: 5000 },
+{ imagen: '🖥️', codigo: 2, tipo: 'Online training', clase: '(pack 8 clases)', valor: 6000 },
+{ imagen: '🥗', codigo: 3, tipo: 'Healthy-nutri', clase: '(plan nutricional)', valor: 4500 },
+{ imagen: '📜', codigo: 4, tipo: 'Healthy-rutina', clase: '(plan de entrenamiento)', valor: 6500 },
+{ imagen: '🏋️', codigo: 5, tipo: 'Programa Healthy Fit', clase: '(programa de 90 dias)', valor: 20000 },
+{ imagen: '🌍', codigo: 6, tipo: 'Personalizado-Online', clase: '(pack 4 clases)', valor: 10000 },
+{ imagen: '🏡', codigo: 7, tipo: 'Personalizado-Domicilio', clase: '(pack 4 clases)', valor: 12000 }]
 
-function aranceles() {
-    let resultado = prompt(mensajeInicial).toLowerCase().trim()
-    if (resultado !== "a" && resultado !== "b" && resultado !== "c" && resultado !== "d") {
-        alert("⛔ El código ingresado no es valido")
+const mensajeInicial = "Selecciona el servicio por el código numérico:"
+
+
+
+function buscarServicio(codigo) {
+    let resultado = servicios.find((servicio) => servicio.codigo === parseInt(codigo))
+    return resultado
+}
+
+function finalizarCompra() {
+    if (carrito.length === 0) {
+        console.warn("El carrito está vacío.")
+        return
+    }
+
+    const shopping = new Compra(carrito)
+    alert('El costo total es de: $ ' + shopping.obtenerSubtotal())
+
+    let respuesta = confirm("¿Eres soci@ activo?" + "\n ✨ Los socios de nuestro gym tienen un 10% off ✨")
+    if (respuesta === true) {
+        alert('El valor a abonar seria de: $ ' + (shopping.obtenerSubtotal() * 0.90))
+    }
+
+    let respuestaDos = confirm("¿Deseas confirmar tu pago?")
+    if (respuestaDos === true) {
+        alert('✅ Confirmamos tu pago' + "\n Muchas gracias por confiar en nosotros 💪")
+        carrito.length = 0
+    }
+}
+
+function comprar() {
+    let codigo = prompt(mensajeInicial)
+    if (!parseInt(codigo)) {
+        alert("Error en el código ingresado.")
+        let respuesta = confirm("¿Deseas intentar nuevamente?")
+        if (respuesta === true) {
+            comprar()
+        }
+        return
+    }
+
+    let servicioElegido = buscarServicio(codigo)
+    if (servicioElegido === undefined) {
+        alert("Codigo ingresado invalido.")
+        return
+    }
+
+    alert(servicioElegido.imagen + ' ' + servicioElegido.tipo + ' ' + servicioElegido.clase + ' ' + ' - ha sido agregado al carrito.')
+    carrito.push(servicioElegido)
+
+    let respuesta = confirm("¿Deseas agregar otro servicio?")
+    if (respuesta === true) {
+        comprar()
     } else {
-        let promoamigos = confirm("¿Vendrias con un compañer@ a entrenar?")
-        if (promoamigos) {
-            descuento = 0.75
-        } else {
-            descuento = 1
-        }
-        switch (resultado) {
-            case "a":
-                valor = 5000 * descuento
-                alert("El entrenamiento Online tiene un valor de $ " + valor + " al mes.")
-                break
-            case "b":
-                valor = 7000 * descuento
-                alert("El entrenamiento Outdoor tiene un valor de $ " + valor + " al mes.")
-                break
-            case "c":
-                valor = 10000 * descuento
-                alert("El Programa de entrenamiento tiene un valor de $ " + valor + " el trimestre.")
-                break
-            case "d":
-                alert("Para mas información sobre la disponibilidad y aranceles, comunicate por whatsapp!!")
-                break
-            default:
-        }
+        finalizarCompra()
     }
-
 }
 
-function horarios() {
-    let resultado = prompt(mensajeInicial).toLowerCase().trim()
-    if (resultado !== "a" && resultado !== "b" && resultado !== "c" && resultado !== "d") {
-        alert("⛔ El código ingresado no es valido")
-    } else {
-        switch (resultado) {
-            case "a":
-                alert("Online = Martes y Jueves 20hs")
-                break
-            case "b":
-                alert("Outdoor = Lunes y miercoles 19hs")
-                break
-            case "c":
-                alert("Lunes, Miercoles y Viernes de 08 a 12 hs")
-                break
-            case "d":
-                alert("Para mas información sobre la disponibilidad y aranceles, comunicate por whatsapp")
-                break
-            default:
-        }
-    }
 
-}
 
-function preguntarHorarios() {
-    while (continuar) {
-        horarios()
-        continuar = confirm("¿Deseas conocer los horarios de otra disciplina?")
-    }
-    alert(" Gracias por tu visita! 📲 Escribenos al 11 1234 5678")
-}
 
-function preguntarAranceles() {
-    while (continuar) {
-        aranceles()
-        continuar = confirm("¿Deseas conocer los horarios de otra disciplina?")
-    }
-    alert(" Gracias por tu visita! 📲 Escribenos al 11 1234 5678")
-}
